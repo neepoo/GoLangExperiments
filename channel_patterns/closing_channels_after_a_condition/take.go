8afdd32f47799c21e544a767166957cd368dd948
neepoo <neepoowzk@gmail.com>
2024-02-25 19:23:31 +0800

    Implementing Take(n) and closing channels after a condition

---
+++ channel_patterns/closing_channels_after_a_condition/take.go
@@ -0,0 +1,25 @@
+package main
+
+func Take[K any](quit chan struct{}, n int, input <-chan K) <-chan K {
+	output := make(chan K)
+	go func() {
+		defer close(output)
+		moreData := true
+		var msg K
+		for n > 0 && moreData {
+			select {
+			case msg, moreData = <-input:
+				if moreData {
+					output <- msg
+					n--
+				}
+			case <-quit:
+				return
+			}
+		}
+		if n == 0 {
+			close(quit)
+		}
+	}()
+	return output
+}
